629378a539f002231031d1851e540df9ad92a171
chenjian <chenjian@gyenno.com>
2024-08-01 10:33:22 +0800

    refactor(): 修改有效括号的解法[]

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -12,24 +12,27 @@
  * 思路2: 使用两个字符串存索引， 然后反转字符串，判断字符串是否相当
  */
 var isValid = function(s) {
-  const left = ['(', '[', '{'];
-  const right = [')', ']', '}'];
-  const stack = [];
-  for (let item of s) {
-    if (left.includes(item)) {
-      stack.push(left.indexOf(item));
+  const stack = []
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+  let result = true
+  for (let index = 0; index < s.length; index++) {
+    const element = s[index];
+    if (map[element]) {
+      stack.push(element)
     } else {
-      if (stack.length == 0) {
-        return false;
-      }
-      const pop = stack.pop();
-      if (pop !== right.indexOf(item)) {
-        return false;
+      const left = stack.pop()
+      if (map[left] !== element) {
+        result = false
+        break
       }
     }
   }
-  return stack.length === 0;
+  return result && stack.length === 0
 };
 // @lc code=end
 
-console.log(isValid('({})}]'))
+console.log(isValid('({})'))
