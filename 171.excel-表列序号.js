c3fc80fd864a3ec21491e0a9a0b352738e918091
Capchen <18879890225@163.com>
2024-08-21 11:58:01 +0800

    feat(): easy

---
+++ 171.excel-表列序号.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=javascript
+ *
+ * [171] Excel 表列序号
+ */
+
+// @lc code=start
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+  let res = 0;
+  for (let i = 0; i < columnTitle.length; i++) {
+    res = res * 26 + (columnTitle[i].charCodeAt() - 'A'.charCodeAt() + 1);
+  }
+  return res;
+};
+// @lc code=end
+
---
+++ 350.两个数组的交集-ii.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=javascript
+ *
+ * [350] 两个数组的交集 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  const map = new Map();
+  const res = [];
+  for (let i = 0; i < nums1.length; i++) {
+    if(map.has(nums1[i])) {
+      map.set(nums1[i], map.get(nums1[i]) + 1);
+    } else {
+      map.set(nums1[i], 1);
+    }
+  }
+
+  for (let i = 0; i < nums2.length; i++) {
+    if (map.has(nums2[i]) && map.get(nums2[i]) > 0) {
+      res.push(nums2[i]);
+      map.set(nums2[i], map.get(nums2[i]) - 1);
+    }
+  }
+  return res;
+};
+// @lc code=end
+
