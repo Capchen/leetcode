32bbd4325b6b7afc2ca8d6da52fe059950a7b6a9
Capchen <18879890225@163.com>
2024-09-18 17:41:21 +0800

    feat(): 11

---
+++ 11.盛最多水的容器.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ * 双指针，向内移动短板，短板决定容器的容积，改变短板才有可能增大容积
+ * 如果使用暴力法，时间复杂度为O(n^2)
+ */
+var maxArea = function(height) {
+    if (height.length < 2) {
+        return 0
+    }
+    let max = 0
+    let left = 0
+    let right = height.length - 1
+    while (left < right) {
+        let h = Math.min(height[left], height[right])
+        max = Math.max(max, h * (right - left))
+        if (height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+    return max
+};
+// @lc code=end
+
