efca8347914ac85ce9a26370cf35fea83c8efa8f
Capchen <18879890225@163.com>
2024-09-23 10:44:03 +0800

    feat: 添加查找和最小的 K 对数字算法实现

---
+++ 373.查找和最小的-k-对数字.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=373 lang=javascript
+ *
+ * [373] 查找和最小的 K 对数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ * nums1 = [1,7,11], nums2 = [2,4,11], k = 3
+ * 优先队列去处理
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+};
+console.log(kSmallestPairs([1,7,11], [2,9,10], 3))
+
+
+// @lc code=end
+
