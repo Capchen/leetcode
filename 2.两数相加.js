431a3e4c58240dd704e50d32b4fe11993a0ef9cc
Capchen <18879890225@163.com>
2024-10-17 11:30:40 +0800

    refactor(): 两数相加优化版本

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -19,30 +19,52 @@
  * l1 = [2,4,3], l2 = [5,6,4]
  */
 var addTwoNumbers = function(l1, l2) {
-    let start = new ListNode(0);
-    let result = start;
-    let temp = 0;
-    if (l1.val + l2.val >= 10) {
-        temp = 1;
-    }
-    start.val = (l1.val + l2.val) % 10;
-    let next1 = l1.next;
-    let next2 = l2.next;
-    while(next1 || next2 || temp) {
-        let node = new ListNode(0);
-        start.next = node;
-        node.val = (next1 ? next1.val : 0) + (next2 ? next2.val : 0) + temp;
-        if (node.val >= 10) {
-            temp = 1;
-            node.val = node.val % 10;
-        } else {
-            temp = 0;
-        }
-        next1 = next1 ? next1.next : null;
-        next2 = next2 ? next2.next : null;
-        start = node;
-    }
-    return result;
+    // let start = new ListNode(0);
+    // let result = start;
+    // let temp = 0;
+    // if (l1.val + l2.val >= 10) {
+    //     temp = 1;
+    // }
+    // start.val = (l1.val + l2.val) % 10;
+    // let next1 = l1.next;
+    // let next2 = l2.next;
+    // while(next1 || next2 || temp) {
+    //     let node = new ListNode(0);
+    //     start.next = node;
+    //     node.val = (next1 ? next1.val : 0) + (next2 ? next2.val : 0) + temp;
+    //     if (node.val >= 10) {
+    //         temp = 1;
+    //         node.val = node.val % 10;
+    //     } else {
+    //         temp = 0;
+    //     }
+    //     next1 = next1 ? next1.next : null;
+    //     next2 = next2 ? next2.next : null;
+    //     start = node;
+    // }
+    // return result;
+
+    // 简化一下
+    const start = new ListNode()
+	let current = start
+	let temp = 0
+	while(l1 !== null || l2 !== null || temp !== 0) {
+		const x = l1? l1.val : 0
+		const y = l2? l2.val : 0
+		const val = x + y + temp
+		
+		if (val >= 10) {
+			temp = 1
+		} else {
+			temp = 0
+		}
+
+		current.next = new ListNode(val % 10)
+		current = current.next
+		l1 = l1? l1.next : null
+		l2 = l2? l2.next : null
+	}
+	return start.next
 };
 // @lc code=end
 
