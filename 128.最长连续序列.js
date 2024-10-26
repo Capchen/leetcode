2847648089f396db4a6550810e7bb56a738938c7
Capchen <18879890225@163.com>
2024-10-26 14:14:35 +0800

    feat(128): 128新方案

--- 128.最长连续序列.js
+++ 128.最长连续序列.js
@@ -31,3 +31,20 @@ var longestConsecutive = function(nums) {
 longestConsecutive([7,3,0,2,5,8,4,6,0,1])
 // @lc code=end
 
+// 第二种方案，先排序，然后遍历一遍，找到最大长度
+var longestConsecutive = function(nums) {
+    const len = nums.length
+    if(len === 0) return 0
+    const newList = nums.sort((a,b) => a - b)
+    const map = new Map()
+    let max = 1
+    for(let i = 0; i < len; i++) {
+        if (map.has(newList[i] - 1)) {
+            map.set(newList[i], map.get(newList[i] - 1) + 1)
+            max = Math.max(max, map.get(newList[i] - 1) + 1)
+        } else {
+            map.set(newList[i], 1)
+        }
+    }
+    return max
+};
