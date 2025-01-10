54679c6b17337ca0e889302f7fbcccc37b23306f
Capchen <18879890225@163.com>
2025-01-10 17:43:42 +0800

    feat: 560

---
+++ 560.和为-k-的子数组.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=javascript
+ *
+ * [560] 和为 K 的子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum === k) {
+        result += 1;
+      }
+    }
+  }
+  return result;
+};
+subarraySum([1,2,3], 3);
+// @lc code=end
+
